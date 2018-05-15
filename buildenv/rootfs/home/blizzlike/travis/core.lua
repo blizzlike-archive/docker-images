383688900c53c2bda1a4515d82ada61f60717b39
crito <crito@fnordpipe.org>
2018-05-15 09:43:31 +0200

    buildenv: update trusty docker image

--- buildenv/rootfs/home/blizzlike/travis/core.lua
+++ buildenv/rootfs/home/blizzlike/travis/core.lua
@@ -24,7 +24,7 @@ octoflow = {
         '-DUSE_ANTICHEAT=0 ' ..
         '-DUSE_EXTRACTORS=1 ' ..
         '-DUSE_LIBCURL=1 ' ..
-        '-DDEBUG=1 ' ..
+        '-DDEBUG=0 ' ..
         '-DCMAKE_INSTALL_PREFIX=' .. os.getenv('CORE_DIR') .. '/core',
       workdir = W .. '/build'
     },
@@ -44,33 +44,41 @@ octoflow = {
 
 function octoflow.build(self)
   print('Starting build #' .. travis.env.build.number)
+  if DISTRO == 'trusty' then
+    octoflow.commands.cmake.cmd = octoflow.commands.cmake.cmd ..
+      ' -DCMAKE_CXX_COMPILER=g++-6' ..
+      ' -DCMAKE_C_COMPILER=gcc-6'
+  end
+
   travis:execute(octoflow.commands.cmake)
   travis:execute(octoflow.commands.make)
   travis:execute(octoflow.commands.make_install)
   travis:execute(octoflow.commands.tarxz)
 
-  if travis.env.type ~= 'pull_request' and
-      travis.env.branch == 'master' and not travis.env.tag then
-    s3:sync(W .. '/packages', octoflow.s3.bucket .. '/' .. travis.env.build.number)
-    local latest = github:get_release({ name = 'latest' })
-
-    if latest then github:delete_release(latest.id) end
-    github:create_release(
-      (latest or { tag_name = 'latest' }).tag_name,
-      (latest or { target_commitish = 'master' }).target_commitish,
-      (latest or { name = 'latest'}).name,
-      nil, true, false)
-  end
+  s3:sync(W .. '/packages', octoflow.s3.bucket .. '/' .. travis.env.build.number)
 end
 
 function octoflow.publish(self)
+  s3:sync(octoflow.s3.bucket .. '/' .. travis.env.build.number, W .. '/packages')
+
   if travis.env.type ~= 'pull_request' and
-      travis.env.branch == 'master' and travis.env.tag then
-    print('Publish packages of ' .. travis.env.tag)
-    s3:sync(octoflow.s3.bucket .. '/' .. travis.env.build.number, W .. '/packages')
-    local tag = github:get_release({ name = travis.env.tag })
-    if tag then github:publish(tag.id, W .. '/packages') end
+      travis.env.branch == 'master' then
+    if not travis.env.tag then
+      print('Tag the latest commit')
+      local latest = github:get_release({ name = 'latest' })
+      if latest then github:delete_release(latest.id) end
+      github:create_release(
+        (latest or { tag_name = 'latest' }).tag_name,
+        (latest or { target_commitish = 'master' }).target_commitish,
+        (latest or { name = 'latest' }).name,
+        nil, true, false)
+    else
+      print('Publish packages of ' .. travis.env.tag)
+      local tag = github:get_release({ name = travis.env.tag })
+      if tag then github:publish(tag.id, W .. '/packages') end
+    end
   end
+
   s3:clear(octoflow.s3.bucket .. '/' .. travis.env.build.number)
 end
 
